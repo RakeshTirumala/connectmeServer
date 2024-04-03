bebb2c8bdc0841300fb9b95f59dbca5f93d33329
Pavan <pjrspavankumar1997@gmail.com>
2024-04-02 21:31:52 -0400

    res

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
+<<<<<<< HEAD
     firstName: { type: String},
     lastName: { type: String},
     email: { type: String, required: true },
@@ -15,12 +16,34 @@ const userSchema = mongoose.Schema({
       liked: [],
       commented: [],
       posted: []
+=======
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  mobile: { type: String },
+  newUser: { type: Boolean, required: true },
+  Education: [],
+  WorkExperience: [],
+  Connections: [],
+  Projects: [
+    {
+      name: { type: String, required: true },
+      description: { type: String, required: true },
+>>>>>>> aa47c79 (test)
     },
-    Interests: [String],
-    dp: { type: Buffer},
-    userType:{type:String},
-  });
-  
-  const User = mongoose.model('User', userSchema);
-  
-  module.exports = User;
+  ],
+  Activity: {
+    liked: [],
+    commented: [],
+    posted: [],
+  },
+  Interests: [String],
+  dp: { type: Buffer },
+  userType: { type: String },
+  Requests: [],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
